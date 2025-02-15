c70961817b98d7cb0ce8f62e6b4947b3ea55c47f
Camilo Rodriguez <arodriguez@cesde.net>
2025-02-15 16:37:44 -0500

    rebuilding some structures

---
+++ js/ToDo.js
@@ -0,0 +1,31 @@
+class ToDo {
+    constructor(nameTask, priority, ) {
+        self.name = nameTask
+        self.priority = priority
+
+    }
+
+    createDo() {
+        if (nameTask.value) {
+            let tareaNueva = document
+        }
+    }
+
+    reviewDo() {
+        
+    }
+
+    deleteDo() {
+        
+    }
+
+    editDo() {
+        
+    }
+
+    remarkDo() {
+        
+    }
+}
+
+export default ToDo
---
+++ js/main.js
@@ -0,0 +1,5 @@
+import ToDo from "module";
+
+const nameTask = document.getElementById("nameTask")
+const createDo = document.getElementById("createdo")
+const listToDo = document.getElementById("listToDo")
