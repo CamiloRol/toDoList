32e5a934434b0201a90c1b03eef069804d5c189d
Camilo Rodriguez <arodriguez@cesde.net>
2025-02-16 17:18:09 -0500

    bootstrap-icons and ToDo class first step

--- js/ToDo.js
+++ js/ToDo.js
@@ -1,22 +1,50 @@
 class ToDo {
-    constructor(nameTask, priority, ) {
-        self.name = nameTask
-        self.priority = priority
-
+    constructor(nameTask, priority, listToDo) {
+        this.name = nameTask
+        this.priority = priority
+        this.listToDo = listToDo
     }
 
     createDo() {
         if (nameTask.value) {
-            let tareaNueva = document
+            let tareaNueva = document.createElement('div')
+            tareaNueva.classList.add('task')
+
+            let texto = document.createElement('p')
+            texto.innerText = nameTask.value
+            tareaNueva.appendChild(texto);
+  
+            // Crear y agregar contenedor de los iconos
+            let iconos = document.createElement('div');
+            iconos.classList.add('icono'); 
+            tareaNueva.appendChild(iconos);
+        
+            // Crear y agregar iconos.
+            let completar = document.createElement('i');
+            completar.classList.add('bi', 'bi-check-circle-fill', 'icono-completar');
+            completar.addEventListener('click', this.reviewDo);
+        
+            let eliminar = document.createElement('i');
+            eliminar.classList.add('bi', 'bi-trash3-fill', 'icono-eliminar');
+            eliminar.addEventListener('click', this.deleteDo);
+        
+            iconos.append(completar, eliminar);
+        
+            // Agregar la tarea a la lista.
+            listToDo.appendChild(tareaNueva);
+        } else {
+            alert('Por favor ingresa una tarea.');
         }
     }
 
-    reviewDo() {
-        
+    reviewDo(e) {
+        let tarea = e.target.parentNode.parentNode;
+        tarea.classList.toggle('completada');
     }
 
-    deleteDo() {
-        
+    deleteDo(e) {
+        let tarea = e.target.parentNode.parentNode;
+        tarea.remove();
     }
 
     editDo() {
--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-import ToDo from "module";
+import ToDo from "./ToDo.js";
 
 const nameTask = document.getElementById("nameTask")
-const createDo = document.getElementById("createdo")
+const createBtn = document.getElementById("createBtn")
 const listToDo = document.getElementById("listToDo")
+const obj = new ToDo(nameTask, listToDo)
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("a").forEach(link => {
+        if (link.getAttribute("href").includes(".html")) {
+            link.addEventListener("click", (event) => {
+                event.preventDefault();
+                const href = link.getAttribute("href")
+
+                document.body.classList.add("fade-out")
+                setTimeout(() => {
+                    window.location.href = href
+                }, 1000)
+            })
+        }
+    })
+})
+
+createBtn.addEventListener('click', () => {
+    obj.createDo()
+})
+
+nameTask.addEventListener('keydown', (e) => {
+  if (e.key == 'Enter') {
+    
+  }
+})
